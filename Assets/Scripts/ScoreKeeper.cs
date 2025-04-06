7cc50b41d163b63c2c9c0657bf4367294b87ff07
tadada50 <tadada50@hotmail.com>
2025-04-05 22:39:51 -0700

    Update score UI

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    [SerializeField] Slider leftPlayerHomeSlider;
+    [SerializeField] Slider rightPlayerHomeSlider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
