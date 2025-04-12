a1da6c355993b11c5eb759e192911bea42ebbd25
tadada50 <tadada50@hotmail.com>
2025-04-12 13:05:09 -0700

    sorting layer fix in progress

--- Assets/Scripts/SetSortingLayers.cs
+++ Assets/Scripts/SetSortingLayers.cs
@@ -9,23 +9,40 @@ public class SetSortingLayers : MonoBehaviour
     {
         
     }
-
+    private void Update()
+    {
+        
+        List<GameObject> playgroundObjects = GetPlaygroundObjects();
+        List<GameObject> leftPlayerBody = playgroundObjects.FindAll(obj => obj.CompareTag("LeftPlayerBody"));
+        SortObjectsByYPosition(playgroundObjects);
+        ProcessBombLayers(playgroundObjects);
+    }
     // Update is called once per frame
-    void Update()
+    private void Update_()
+    {
+        List<GameObject> playgroundObjects = GetPlaygroundObjects();
+        SortObjectsByYPosition(playgroundObjects);
+        ProcessBombLayers(playgroundObjects);
+    }
+    
+    private List<GameObject> GetPlaygroundObjects()
     {
         List<GameObject> playgroundObjects = new List<GameObject>();
         GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
 
-        int baseOrder = 0;
-        int skip = 0;
         foreach (GameObject obj in allObjects)
         {
             SpriteRenderer renderer = obj.GetComponent<SpriteRenderer>();
             if (renderer != null && renderer.sortingLayerName == "Playground")
             {
-            playgroundObjects.Add(obj);
+                playgroundObjects.Add(obj);
             }
         }
+        return playgroundObjects;
+    }
+
+    private void SortObjectsByYPosition(List<GameObject> playgroundObjects)
+    {
         playgroundObjects.Sort((a, b) => b.transform.position.y.CompareTo(a.transform.position.y));
         for (int i = 0; i < playgroundObjects.Count; i++)
         {
@@ -35,50 +52,49 @@ public class SetSortingLayers : MonoBehaviour
                 renderer.sortingOrder = i;
             }
         }
+    }
+
+    private void ProcessBombLayers(List<GameObject> playgroundObjects)
+    {
+        int baseOrder = 0;
+        int skip = ProcessPlayerBombs(ref baseOrder);
+        int bombBodySkip = ProcessBombBodies(playgroundObjects);
+        AdjustRemainingLayers(playgroundObjects, baseOrder, skip, bombBodySkip);
+    }
+
+    private int ProcessPlayerBombs(ref int baseOrder)
+    {
+        int skip = 0;
         foreach (GameObject player in players)
         {
             PlayerMovement playerMovement = player.GetComponentInChildren<PlayerMovement>();
-            if (playerMovement != null && playerMovement.mBomb!=null)
+            if (playerMovement != null && playerMovement.mBomb != null)
             {
                 SpriteRenderer[] bombRenderer = playerMovement.mBomb.GetComponentsInChildren<SpriteRenderer>();
-                SpriteRenderer playerRenderers = player.GetComponentInChildren<SpriteRenderer>();
-                baseOrder = playerRenderers.sortingOrder;
+                SpriteRenderer playerRenderer = player.GetComponentInChildren<SpriteRenderer>();
+                baseOrder = playerRenderer.sortingOrder;
                 foreach (SpriteRenderer renderer in bombRenderer)
                 {
                     if (renderer.sortingLayerName == "Playground")
                     {
                         skip++;
                         renderer.sortingOrder = baseOrder + skip;
-
                     }
                 }
-
-
-
-                // ParticleSystem[] particleSystems = playerMovement.bomb.GetComponentsInChildren<ParticleSystem>();
-                // foreach (ParticleSystem particleSystem in particleSystems)
-                // {
-                //     ParticleSystemRenderer particleRenderer = particleSystem.GetComponent<ParticleSystemRenderer>();
-                //     if (particleRenderer != null && particleRenderer.sortingLayerName == "Playground")
-                //     {
-                //         skip++;
-                //         particleRenderer.sortingOrder = baseOrder + skip;
-                //     }
-                // }
-
-
-                // Debug.Log($"BaseOrder: {baseOrder}, Skip: {skip}");
             }
         }
+        return skip;
+    }
 
-        int bombBodyOrder=0;
-        int bombBodySkip=0;
+    private int ProcessBombBodies(List<GameObject> playgroundObjects)
+    {
+        int bombBodySkip = 0;
         foreach (GameObject obj in playgroundObjects)
         {
             SpriteRenderer renderer = obj.GetComponent<SpriteRenderer>();
-            if(obj.name=="BombBody")
+            if (obj.name == "BombBody")
             {
-                bombBodyOrder = renderer.sortingOrder;
+                int bombBodyOrder = renderer.sortingOrder;
                 ParticleSystem[] particleSystems = obj.GetComponentsInChildren<ParticleSystem>();
                 foreach (ParticleSystem particleSystem in particleSystems)
                 {
@@ -90,23 +106,19 @@ public class SetSortingLayers : MonoBehaviour
                     }
                 }
             }
-           // Debug.Log($"Object: {obj.name}, Order: {renderer.sortingOrder}, Y Position: {obj.transform.position.y}");
         }
-        for(int i = baseOrder+1; i < playgroundObjects.Count; i++)
+        return bombBodySkip;
+    }
+
+    private void AdjustRemainingLayers(List<GameObject> playgroundObjects, int baseOrder, int skip, int bombBodySkip)
+    {
+        for (int i = baseOrder + 1; i < playgroundObjects.Count; i++)
         {
             SpriteRenderer renderer = playgroundObjects[i].GetComponent<SpriteRenderer>();
-
-            if (renderer != null && renderer.name!="Bomb" && renderer.name!="BombBody")
+            if (renderer != null && renderer.name != "Bomb" && renderer.name != "BombBody")
             {
                 renderer.sortingOrder += skip + bombBodySkip;
             }
-        }   
-
-        foreach (GameObject obj in playgroundObjects)
-        {
-            SpriteRenderer renderer = obj.GetComponent<SpriteRenderer>();
-           // Debug.Log($"Object: {obj.name}, Order: {renderer.sortingOrder}, Y Position: {obj.transform.position.y}");
         }
-
     }
 }
